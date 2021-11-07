161bbfe07b4af4a8df67a97bd74f97bbaa626278
apc518 <andychamberlainmusic@gmail.com>
2021-11-06 17:01:48 -0700

    added win/tie detection and exit command

--- src/main.rs
+++ src/main.rs
@@ -20,19 +20,25 @@ fn main() {
         io::stdin()
             .read_line(&mut user_move)
             .expect("Failed to read line");
-
-        println!("Your move was {}", user_move);
+        
+        if user_move == "exit\n" {
+            break;
+        }
 
         let move_tuple = parse_move(user_move);
 
+        // if move is out of bounds
         if move_tuple.0 > 2 || move_tuple.1 > 2 {
             println!("Invalid input.");
             continue;
         }
+        // if move is to an already occupied square
+        if board[move_tuple.0][move_tuple.1] != '-' {
+            println!("Invalid move. That square is already occupied.");
+            continue;
+        }
 
-        println!("Parsed: {}, {}", move_tuple.0, move_tuple.1);
-
-        board[move_tuple.0][move_tuple.1] = TURN_CHARS[turn];
+        board[move_tuple.0][move_tuple.1] = TURN_CHARS[turn % 2];
 
         println!("Board:");
         for elem in board {
@@ -42,7 +48,16 @@ fn main() {
             print!("\n");
         }
 
-        turn = (turn + 1) % 2;
+        if WIN_CHECK_FUNCS[move_tuple.0][move_tuple.1](board) {
+            println!("{} wins!", TURN_CHARS[turn % 2]);
+            break;
+        }
+        else if turn >= 8 {
+            println!("Tie.");
+            break;
+        }
+
+        turn += 1;
     }
 }
 
@@ -59,3 +74,69 @@ fn parse_move(s: String) -> (usize, usize) {
 
     (row, col)
 }
+
+static WIN_CHECK_FUNCS: [[fn([[char;3];3]) -> bool; 3];3] = [
+    [
+        |board: [[char;3];3]| { 
+            board[0][0] == board[1][1] && board[1][1] == board[2][2] // backslash diagonal (\)
+            ||
+            board[0][0] == board[0][1] && board[0][1] == board[0][2] // top row
+            ||
+            board[0][0] == board[1][0] && board[1][0] == board[2][0] // left column
+        },
+        |board: [[char;3];3]| {
+            board[0][0] == board[0][1] && board[0][1] == board[0][2] // top row
+            ||
+            board[0][1] == board[1][1] && board[1][1] == board[2][1] // middle column
+        },
+        |board: [[char;3];3]| {
+            board[0][2] == board[1][1] && board[1][1] == board[2][0] // forward slash diagonal (/)
+            ||
+            board[0][0] == board[0][1] && board[0][1] == board[0][2] // top row
+            ||
+            board[0][2] == board[1][2] && board[1][2] == board[2][2] // right column
+        }
+    ],
+    [
+        |board: [[char;3];3]| {
+            board[0][0] == board[1][0] && board[1][0] == board[2][0] // left column
+            ||
+            board[1][0] == board[1][1] && board[1][1] == board[1][2] // middle row
+        },
+        |board: [[char;3];3]| {
+            board[0][2] == board[1][1] && board[1][1] == board[2][0] // forward slash diagonal (/)
+            ||
+            board[0][0] == board[1][1] && board[1][1] == board[2][2] // backslash diagonal (\)
+            ||
+            board[0][1] == board[1][1] && board[1][1] == board[2][1] // middle column
+            ||
+            board[1][0] == board[1][1] && board[1][1] == board[1][2] // middle row
+        },
+        |board: [[char;3];3]| {
+            board[0][2] == board[1][2] && board[1][2] == board[2][2] // right column
+            ||
+            board[1][0] == board[1][1] && board[1][1] == board[1][2] // middle row
+        }
+    ],
+    [
+        |board: [[char;3];3]| {
+            board[0][2] == board[1][1] && board[1][1] == board[2][0] // forward slash diagonal (/)
+            ||
+            board[0][0] == board[1][0] && board[1][0] == board[2][0] // left column
+            ||
+            board[2][0] == board[2][1] && board[2][1] == board[2][2] // bottom row
+        },
+        |board: [[char;3];3]| {
+            board[0][1] == board[1][1] && board[1][1] == board[2][1] // middle column
+            ||
+            board[2][0] == board[2][1] && board[2][1] == board[2][2] // bottom row
+        },
+        |board: [[char;3];3]| {
+            board[0][0] == board[1][1] && board[1][1] == board[2][2] // backslash diagonal (\)
+            ||
+            board[2][0] == board[2][1] && board[2][1] == board[2][2] // bottom row
+            ||
+            board[0][2] == board[1][2] && board[1][2] == board[2][2] // right column
+        }
+    ]
+];
